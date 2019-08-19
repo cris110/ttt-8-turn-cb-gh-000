82e7ec879bbf1e74f7965471032079e0d5387d0f
Cristiano Moreira <cristiano.moreira01@fatec.sp.gov.br>
2019-08-19 03:34:25 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -15,7 +15,7 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
-  input = gets.to_i
+  input = gets.strip.to_i
   index = input_to_index(input)
   if index.between?(0,8)
     move(board, index)
