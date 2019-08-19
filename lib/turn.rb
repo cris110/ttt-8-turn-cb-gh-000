cdf797871eee4911dce86f647923ab2082dcf156
Cristiano Moreira <cristiano.moreira01@fatec.sp.gov.br>
2019-08-19 02:48:30 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,3 +6,8 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
   puts "-----------"
 end
+
+def valid_move?(board, index)
+  if index.between?(0,8) && (board[index] == "" || board[index] == " " || board[index] == nil)
+    true
+end
